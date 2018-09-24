76dbd6c195a4fc9b71db080ba56b9be49c5d3ded
L200160037 <43530476+L200160037@users.noreply.github.com>
2018-09-24 15:18:40 +0700

    Add files via upload

---
+++ TugasNo1_MODUL1.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>Background Gambar</title>
+	</head>
+	<body background='L200160037.jpg'>
+		<h1>Biodata Diri:</h1>
+		<h2>Nama : Rizky Ericka Pratami Widodo<br>
+		NIM : L200160037<br>
+		Kota : Karanganyar</h2>
+	</body>
+</html>
